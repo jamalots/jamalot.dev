44ff4c4f9e9e4ef6c9153aa0e7c06d0b227c2c8c
Tom Young <tom.young.satx@gmail.com>
2015-09-23 15:40:57 -0500

    audio player

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -1,1 +1,11 @@
-/vagrant/sites/jamalot.dev/app/views/users/show.blade.php
+@extends('layouts.master')
+
+@section('content')
+<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/147853477&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe><script src="//connect.soundcloud.com/sdk-2.0.0.js"></script>
+
+
+// initialize client with app credentials
+
+
+
+@stop
