df68d92c90bdfb301d5b039bdf85067f625104cb
Lorne <lorne21@gmail.com>
2015-09-23 09:46:50 -0500

    user model change

--- app/models/User.php
+++ app/models/User.php
@@ -5,7 +5,7 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
-use Eloquent;
+// use Eloquent;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
