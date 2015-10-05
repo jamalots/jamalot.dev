a229c12f559ecd7f5f8b428be9b3e42e9aa2cda0
Christian Tobias <cwtobias6@gmail.com>
2015-10-05 13:43:03 -0500

    comments work but need validation for auth user

--- app/views/notifications/partials/notification.blade.php
+++ app/views/notifications/partials/notification.blade.php
@@ -5,29 +5,32 @@
 	
 
 	<div class="media-body">
-		@if( $notification->source_user->user_type == 'Band')
-			@if( $notification->notification_type == 'following')
-				<p>{{ $notification->source_user->band_name }} is now following you.</p>
-				<p>{{ $notification->created_at->diffForHumans() }}</p>
-			@elseif( $notification->notification_type == 'registered')
-				<p>{{ $notification->source_user->band_name }} registered for your event.</p>
-				<p>{{ $notification->created_at->diffForHumans() }}</p>
-			@elseif( $notification->notification_type == 'commented')
-				<p>{{ $notification->source_user->band_name }} commented on your status.</p>
-				<p>{{ $notification->created_at->diffForHumans() }}</p>	
+
+	
+			@if( $notification->source_user->user_type == 'Band')
+				@if( $notification->notification_type == 'following')
+					<p>{{ $notification->source_user->band_name }} is now following you.</p>
+					<p>{{ $notification->created_at->diffForHumans() }}</p>
+				@elseif( $notification->notification_type == 'registered')
+					<p>{{ $notification->source_user->band_name }} registered for your event.</p>
+					<p>{{ $notification->created_at->diffForHumans() }}</p>
+				@elseif( $notification->notification_type == 'commented')
+					<p>{{ $notification->source_user->band_name }} commented on your status.</p>
+					<p>{{ $notification->created_at->diffForHumans() }}</p>	
+				@endif
+			@elseif($notification->source_user->user_type == 'Musician')
+				@if( $notification->notification_type == 'following')
+					<p>{{ $notification->source_user->first_name }} {{ $notification->source_user->last_name }} is now following you.</p>
+					<p>{{ $notification->created_at->diffForHumans() }}</p>
+				@elseif( $notification->notification_type == 'registered')
+					<p>{{ $notification->source_user->first_name }} {{ $notification->source_user->last_name }} registered for your event.</p>
+					<p>{{ $notification->created_at->diffForHumans() }}</p>
+				@elseif( $notification->notification_type == 'commented')
+					<p>{{ $notification->source_user->first_name }} {{ $notification->source_user->last_name }} commented on your status.</p>
+					<p>{{ $notification->created_at->diffForHumans() }}</p>	
+				@endif
 			@endif
-		@elseif($notification->source_user->user_type == 'Musician')
-			@if( $notification->notification_type == 'following')
-				<p>{{ $notification->source_user->first_name }} {{ $notification->source_user->last_name }} is now following you.</p>
-				<p>{{ $notification->created_at->diffForHumans() }}</p>
-			@elseif( $notification->notification_type == 'registered')
-				<p>{{ $notification->source_user->first_name }} {{ $notification->source_user->last_name }} registered for your event.</p>
-				<p>{{ $notification->created_at->diffForHumans() }}</p>
-			@elseif( $notification->notification_type == 'commented')
-				<p>{{ $notification->source_user->first_name }} {{ $notification->source_user->last_name }} commented on your status.</p>
-				<p>{{ $notification->created_at->diffForHumans() }}</p>	
-			@endif
-		@endif
+		
 	</div>
 
 </article>
